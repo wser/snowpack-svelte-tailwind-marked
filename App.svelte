964746ec30b553afccd6f018a9e01a81a7b7d008
wser <w.web.dev@gmail.com>
2021-01-30 22:03:30 +0100

    added svelte App to index.js

---
+++ App.svelte
@@ -0,0 +1,21 @@
+<!-- App.svelte -->
+<script>
+  /* component logic will go here */
+</script>
+
+<div class="App">
+  <header class="App-header">
+    <a
+      class="App-link"
+      href="https://svelte.dev"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn Svelte
+    </a>
+  </header>
+</div>
+
+<style>
+  /* css will go here */
+</style>
